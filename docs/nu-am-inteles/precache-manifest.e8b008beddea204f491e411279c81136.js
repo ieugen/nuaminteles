fcd49155a5c6f23e48e1a5839cf4d43dcb4143da
Ioan Eugen Stan <eu@ieugen.ro>
2018-11-25 17:50:20 +0200

    Added app

---
+++ docs/nu-am-inteles/precache-manifest.e8b008beddea204f491e411279c81136.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "74a3efe16d12a86e5695",
+    "url": "/static/js/main.74a3efe1.chunk.js"
+  },
+  {
+    "revision": "f112cf4a0332fe044047",
+    "url": "/static/js/1.f112cf4a.chunk.js"
+  },
+  {
+    "revision": "74a3efe16d12a86e5695",
+    "url": "/static/css/main.e4df03f0.chunk.css"
+  },
+  {
+    "revision": "0feff72b2d6f652b9e9d8ed24d677838",
+    "url": "/index.html"
+  }
+];
